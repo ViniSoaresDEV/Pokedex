a7842f068be820250c6180c6c2e3b0dc6e6adf7f
ViniSoaresDEV <viniciussilvaaa@live.com>
2025-09-05 16:41:38 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const btn = document.querySelector('button#btn');
+const input = document.querySelector('input#input');
+const img = document.querySelector('img#img');
+const backgroundImageColor = document.querySelector('div#backgroundImage');
+const backgroundTypeColor = document.querySelector('div.pokemonType');
+const pokemonName = document.querySelector('h3#pokemonName');
+const pokemonType = document.querySelector('p#pokemonType');
+const pokemonCode = document.querySelector('p#pokemonCode');
+const pokemonHP = document.querySelector('p#pokemonHpStatus');
+const pokemonAttack = document.querySelector('p#pokemonAttackStatus');
+const pokemonDefense = document.querySelector('p#pokemonDefenseStatus');
+
+btn.addEventListener('click', ()=>{
+    if(input.value === '' || input.value === null) alert('Digite o nome do Pokémon.');
+
+    searchPokemon(input.value.toLowerCase());
+
+})
+
+async function searchPokemon(name) {
+    try{
+        const urlPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+
+        const responsePokemon = await urlPokemon.json();
+        showPokemon(responsePokemon);
+        changeBackground(responsePokemon);
+    }catch(error){
+        pokemonName.innerHTML = 'Pokémon não encontrado';
+        console.log('Pokémon não encontrado', error)
+    }
+}
+
+function showPokemon(pokemon){
+    img.src = pokemon.sprites.front_default;
+    pokemonName.innerHTML = pokemon.name.toUpperCase();
+    pokemonType.innerHTML = pokemon.types[0].type.name;
+    pokemonCode.innerHTML = `#${pokemon.id}`;
+    pokemonHP.innerHTML = pokemon.stats[0].base_stat;
+    pokemonAttack.innerHTML = pokemon.stats[1].base_stat;
+    pokemonDefense.innerHTML = pokemon.stats[2].base_stat;
+}
+
+function changeBackground(pokemon){
+    const type = pokemon.types[0].type.name;
+
+    //RESET CLASS
+
+    resetClass(backgroundImageColor);
+    resetClass(backgroundTypeColor);
+    
+    //ADD CLASS
+    backgroundImageColor.classList.add(`type-${type}`);
+    backgroundTypeColor.classList.add(`type-${type}`);
+}
+
+//reset class function
+function resetClass(sendClass){
+    sendClass.classList.forEach(cls => {
+        if(cls.startsWith("type-")){
+            sendClass.classList.remove(cls);
+        }
+    });
+}
+
+
+
